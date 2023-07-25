e0e07294e3e4e9618b155f30a3b063d742c2b777
UtpalMaiti <Utpalmaiti12@gmail.com>
2023-07-25 17:46:52 +0530

    TT

--- ASPNET core Single Page Web Application React v8.x/App,ALL/8.1.0/aspnet-core/src/App,ALL.Application/Authorization/Accounts/Dto/RegisterOutput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace App,ALL.Authorization.Accounts.Dto
-{
-    public class RegisterOutput
-    {
-        public bool CanLogin { get; set; }
-    }
-}
--- ASPNET core Single Page Web Application React v8.x/App,ALL/8.1.0/aspnet-core/src/App,ALL.Application/Authorization/Accounts/Dto/TenantAvailabilityState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App,ALL.Authorization.Accounts.Dto
-{
-    public enum TenantAvailabilityState
-    {
-        Available = 1,
-        InActive,
-        NotFound
-    }
-}
